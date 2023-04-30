4ecd83a929649f98e777e0e50a1b7d9b72bf7a0b
Amani <amalmutairi22@twq.idserve.net>
2023-04-30 09:37:58 +0300

    Initial Commit

---
+++ Lulu App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Lulu App
+//
+//  Created by Amani Almutairi  on 10/10/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lulu App/Lulu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lulu_AppApp.swift
+//  Lulu App
+//
+//  Created by Amani Almutairi  on 10/10/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Lulu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
